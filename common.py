dba3751c81cf85ce1c2dfc9e4fc11de4d9cc5a4e
yhsong12345 <145076893+yhsong12345@users.noreply.github.com>
2023-09-21 17:04:44 +0900

    Update common.py

--- common.py
+++ common.py
@@ -4,53 +4,6 @@ import torch.nn.functional as F
 import math
 
 
-class LambdaLayer(nn.Module):
-    def __init__(self, lambd):
-        super(LambdaLayer, self).__init__()
-        self.lambd = lambd
-    
-    def forward(self, x):
-        return self.lambd(x)
-
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-    def __init__(self, in_planes, planes, stride=1, option='A'):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, 
-            stride=stride, padding=1, bias=False
-        )
-
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != planes:
-            if option == 'A':
-                self.shortcut = LambdaLayer(lambda x:
-                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), 'constant', 0))
-            if option == 'B':
-                self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-                )
-
-    
-    def forward(self, x):
-        o = F.relu(self.bn1(self.conv1(x)))
-        o = self.bn2(self.conv2(o))
-        o += self.shortcut(x)
-        o = F.relu(o)
-
-        return o
-
-
 
 class Bottleneck(nn.Module):
     expansion=4
